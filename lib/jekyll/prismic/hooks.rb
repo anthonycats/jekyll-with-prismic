57f64413011b39d19b11f845dd0119c1f2d14fe7
Loïc Fürhoff <imagoiq@posteo.net>
2018-01-30 11:27:20 +0100

    Add custom hooks data

--- lib/jekyll/prismic/hooks.rb
+++ lib/jekyll/prismic/hooks.rb
@@ -2,7 +2,7 @@ module Jekyll
   module Prismic
     # Add Prismic Liquid variables to all templates
     Jekyll::Hooks.register :site, :pre_render do |site, payload|
-      payload['site']['prismic'] = PrismicDrop.new(site)
+      payload['site']['prismicData'] = PrismicDrop.new(site)
     end
 
     Jekyll::Hooks.register :site, :after_reset do |site|
